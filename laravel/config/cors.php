a66d9176674c331c548efedd8db7c06e25cb7388
Ryan Durham <ryan@stagerightlabs.com>
2018-01-03 20:36:12 -0800

    Install Laravel-CORS pacakge and remove CSRF check from registration endpoint.

---
+++ laravel/config/cors.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+
+    'supportsCredentials' => false,
+    'allowedOrigins' => [getenv('FRONTEND_URL')],
+    'allowedHeaders' => ['Content-Type', 'X-Requested-With'],
+    'allowedMethods' => ['GET','POST','PUT', 'OPTIONS'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+
+];
