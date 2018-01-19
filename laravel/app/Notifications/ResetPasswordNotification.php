4bc352beb52cfe9b6d20e2a6f5427aa43cfa42ac
Ryan Durham <ryan@stagerightlabs.com>
2018-01-18 19:55:46 -0800

    Fix password reset link in email template.

--- laravel/app/Notifications/ResetPasswordNotification.php
+++ laravel/app/Notifications/ResetPasswordNotification.php
@@ -47,7 +47,7 @@ class ResetPasswordNotification extends Notification
      */
     public function toMail($notifiable)
     {
-        $resetUrl = env('FRONTEND_URL') . route('password.reset', $this->token, false);
+        $resetUrl = env('FRONTEND_URL') . "/password/reset/" . $this->token;
 
         return (new MailMessage)
                     ->line('The introduction to the notification.')
